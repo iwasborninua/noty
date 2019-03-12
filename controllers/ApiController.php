77519857636bdb8bac65c2f69c2dec0150147e1b
McNamara <ololo@mail.ri>
2019-03-12 16:18:09 +0200

    add api controller

---
+++ controllers/ApiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Author;
+use app\models\Book;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class ApiController extends Controller
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionBooksList() {
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        return Books::find()
+            ->with('author')
+            ->all();
+    }
+}
